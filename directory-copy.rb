ac7880ebd9f91b2672c11f2b9af4ca1a4d8188ec
Ian <iantfleming1992@gmail.com>
2020-11-12 15:12:32 +0000

    exercise 8(2)

--- directory-copy.rb
+++ directory-copy.rb
@@ -1,29 +1,38 @@
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
     
-    students = []
+  students = []
     
-    name = gets.chomp
+  name = gets.chomp
+  
     
-    while !name.empty? do
+  while !name.empty? do
+    if name[0] == 'm' || name[0] == 'M'
+      if name.length <= 12
         students << {name: name, cohort: :november}
         puts "Now we have #{students.count} students"
         name = gets.chomp
+      else
+        puts "Please enter the names of the students"
+        puts "To finish, just hit return twice"
+        name = gets.chomp
+      end
     end
-    students
+  end
+  students
 end
 def print_header    
-    puts 'The students of Villains Academy'
-    puts '------------'
+  puts 'The students of Villains Academy'
+  puts '------------'
 end
 def print(students)
-    students.each_with_index do |student, i|
-        puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  students.each_with_index do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end
 students = input_students
 print_header
