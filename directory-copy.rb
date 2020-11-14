ad3e3d5565031942e41854967e06e063af6c8ea2
Ian <iantfleming1992@gmail.com>
2020-11-14 12:27:27 +0000

    Step 8 complete

--- directory-copy.rb
+++ directory-copy.rb
@@ -1,41 +1,40 @@
 def input_students
-  puts "Please enter the names of the students".center(85)
-  puts "To finish, just hit return twice".center(85)
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
     
-  students = []
-    
+  students = []  
   name = gets.chomp
   
-  puts 'Enter cohort'.center(85)
+  puts 'Enter cohort'
   cohort = gets.chomp
   if cohort.empty?
     cohort = :november
   end
 
-  puts 'Please enter student nationality'.center(85)
+  puts 'Please enter student nationality'
   nation = gets.chomp
     
   while !name.empty?
     students << {name: name, cohort: cohort.to_sym, nationality: nation}
     if students.count == 1
-      puts "Now we have 1 student".center(85)
-      puts "Please enter the names of the students".center(85)
+      puts "Now we have 1 student"
+      puts "Please enter the names of the students"
       name = gets.chomp
       if !name.empty?
-        puts 'Enter cohort'.center(85)
+        puts 'Enter cohort'
         cohort = gets.chomp  
         if cohort.empty?
           cohort = :november
         end               
-        puts 'Please enter student nationality'.center(85)
+        puts 'Please enter student nationality'
         nation = gets.chomp
       end               
     else
-      puts "Now we have #{students.count} students".center(85)
-      puts "Please enter the names of the students".center(85)
+      puts "Now we have #{students.count} students"
+      puts "Please enter the names of the students"
       name = gets.chomp
       if !name.empty?
-        puts 'Enter cohort'.center(85)
+        puts 'Enter cohort'
         cohort = gets.chomp 
         if cohort.empty?
           cohort = :november
@@ -48,32 +47,50 @@ def input_students
   students
 end
 
+def output_by_letter(students)
+  students.each do |student| 
+    puts 'hello'
+    if student[:name][0] == 'm' || student[:name][0] == 'M'
+      puts student[:name]
+    else
+      nil
+    end
+  end
+end
+
+def sort_cohort(students)
+  students.sort_by{|student| student[:cohort]}
+end
+
+
 def print_header    
-  puts 'The students of Villains Academy'.center(85)
-  puts '------------'.center(85)
+  puts 'The students of Villains Academy'
+  puts '------------'
 end
 
 def print_students(students)
-  students.each.with_index do |student, i|
-    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})".center(85)
+  students.each_with_index do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"
   end
 end
 
 def print_footer(names)
   unless names.count < 1
     if names.count == 1
-      puts "Overall, we have 1 great student".center(85)
+      puts "Overall, we have 1 great student"
     else  
-      puts "Overall, we have #{names.count} great students".center(85)
+      puts "Overall, we have #{names.count} great students"
     end
   end
 end
 
 
 students = input_students
+students = sort_cohort(students)
 print_header
 print_students(students)
 print_footer(students)
+output_by_letter(students)
 
 
 # def input_students
