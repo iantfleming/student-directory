6a3ee8b8bf05249681dc6735494cb7e02e107dd0
Ian <iantfleming1992@gmail.com>
2020-11-14 15:43:47 +0000

    Step 9 final

--- directory-copy.rb
+++ directory-copy.rb
@@ -39,7 +39,7 @@ def input_students
         if cohort.empty?
           cohort = :november
         end               
-        puts 'Please enter student nationality'.center(85)
+        puts 'Please enter student nationality'
         nation = gets.chomp
       end                
     end
@@ -91,45 +91,3 @@ print_header
 print_students(students)
 print_footer(students)
 output_by_letter(students)
-
-
-# def input_students
-#   puts "Please enter the names of the students"
-#   puts "To finish, just hit return twice"
-    
-#   students = []
-    
-#   name = gets.chomp
-  
-    
-#   while !name.empty? do
-#     if name[0] == 'm' || name[0] == 'M'
-#       if name.length <= 12
-#         students << {name: name, cohort: :november}
-#         puts "Now we have #{students.count} students"
-#         name = gets.chomp
-#       else
-#         puts "Please enter the names of the students"
-#         puts "To finish, just hit return twice"
-#         name = gets.chomp
-#       end
-#     end
-#   end
-#   students
-# end
-# def print_header    
-#   puts 'The students of Villains Academy'
-#   puts '------------'
-# end
-# def print(students)
-#   students.each_with_index do |student, i|
-#     puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-#   end
-# end
-# def print_footer(names)
-#   puts "Overall, we have #{names.count} great students"
-# end
-# students = input_students
-# print_header
-# print(students)
-# print_footer(students)
