2999b062692d472b87e99a108c9a2e3dc62d011d
Ian <iantfleming1992@gmail.com>
2020-11-15 18:45:35 +0000

    copy

--- directory-copy.rb
+++ directory-copy.rb
@@ -1,3 +1,37 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students  
+    when "2"
+      print_header
+      print_students(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students(students)
+  print_footer(students)
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -84,7 +118,7 @@ def print_footer(names)
   end
 end
 
-
+interactive_menu
 students = input_students
 students = sort_cohort(students)
 print_header
