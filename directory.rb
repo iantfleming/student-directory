6713351aeca68f0b5db114470d00a89b6e2d4afb
Ian <iantfleming1992@gmail.com>
2020-11-15 20:16:09 +0000

    step 13

--- directory.rb
+++ directory.rb
@@ -6,12 +6,12 @@ def input_students
   puts "To finish, just hit return twice"
     
     
-  name = gets.chomp
+  name = STDIN.gets.chomp
     
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -41,7 +41,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -78,7 +78,7 @@ def save_students
   file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(', ')
@@ -86,6 +86,18 @@ def load_students
   end
   file.close
 end
-    
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+    
+try_load_students
 interactive_menu
